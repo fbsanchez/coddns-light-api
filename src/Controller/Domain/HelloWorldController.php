4908501d948dd4df0064a347fba66ae413b27e09
Fco de Borja Sánchez <fborja.sanchezs@gmail.com>
2024-05-22 01:01:33 +0200

    Rc1

---
+++ src/Controller/Domain/HelloWorldController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Domain;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class HelloWorldController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return new Response(json_encode(['hello' => 'world']));
+    }
+
+}
