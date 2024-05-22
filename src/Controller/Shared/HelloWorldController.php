2d93bfb82d89e2df6f9869d3a5021c449c0cbd14
Fco de Borja Sánchez <fborja.sanchezs@gmail.com>
2024-05-22 11:59:04 +0200

    Rc1

--- src/Controller/Shared/HelloWorldController.php
+++ src/Controller/Shared/HelloWorldController.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Controller\Domain;
+namespace App\Controller\Shared;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
